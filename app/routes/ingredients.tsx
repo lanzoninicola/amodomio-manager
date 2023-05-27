509f9e03a8283d2d5de783782040bf2e05510c0c
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2023-05-27 18:10:19 -0300

    feat: add select field on ingredient price

--- app/routes/ingredients.tsx
+++ app/routes/ingredients.tsx
@@ -2,6 +2,7 @@ import type { ActionArgs, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { MoreHorizontal, Trash2 } from "lucide-react";
+import React from "react";
 import Container from "~/components/layout/container/container";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
@@ -10,6 +11,17 @@ import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Separator } from "~/components/ui/separator";
 import { IngredientModel, type Ingredient } from "~/data-access/models/ingredient-model.server";
+import { type IngredientPrice, IngredientPriceModel } from "~/data-access/models/ingredient-price-model.server";
+import { SupplierModel, type Supplier } from "~/data-access/models/supplier-model.server";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "~/components/ui/select"
 
 export const meta: V2_MetaFunction = () => {
     return [
@@ -18,10 +30,18 @@ export const meta: V2_MetaFunction = () => {
     ];
 };
 
+interface LoaderData {
+    ingredients: Ingredient[]
+    prices: IngredientPrice[]
+    suppliers: Supplier[]
+}
+
 export async function loader() {
     const ingredients = await IngredientModel.findAll()
+    const ingredientsPrices = await IngredientPriceModel.findAll()
+    const suppliers = await SupplierModel.findAll()
 
-    return json(ingredients)
+    return json({ ingredients, prices: ingredientsPrices, suppliers })
 }
 
 export async function action({ request }: ActionArgs) {
@@ -54,7 +74,7 @@ export async function action({ request }: ActionArgs) {
 
 
 export default function Index() {
-    const loaderData: Ingredient[] = useLoaderData<typeof loader>()
+    const loaderData: LoaderData = useLoaderData<typeof loader>()
 
     return (
         <Container>
@@ -64,7 +84,7 @@ export default function Index() {
                 </h1>
                 <Form method="post">
                     <Fieldset>
-                        <Label htmlFor="ingredients-name">Nome ingrediente</Label>
+                        <Label htmlFor="ingredients-name">Nome</Label>
                         <Input type="string" id="ingredient-name" placeholder="Nome ingrediente" name="name" />
                     </Fieldset>
                     <Button type="submit" name="_action" value="create-ingredient">Salvar</Button>
@@ -80,10 +100,10 @@ export default function Index() {
                     </CardDescription>
                 </CardHeader>
                 <CardContent className="grid gap-6">
-                    {loaderData.length === 0 && (
+                    {loaderData?.ingredients.length === 0 && (
                         <span>Nenhum ingrediente cadastrado</span>
                     )}
-                    <IngredientList ingredients={loaderData} />
+                    <IngredientList ingredients={loaderData.ingredients} prices={loaderData.prices} />
                 </CardContent>
             </Card>
 
@@ -92,7 +112,10 @@ export default function Index() {
 }
 
 
-function IngredientList({ ingredients }: { ingredients: Ingredient[] }) {
+function IngredientList({ ingredients, prices }: { ingredients: Ingredient[], prices: IngredientPrice[] }) {
+    // Pressing "MoreHorizontal" button should be handled via SSR and not client-side
+    const [showPriceForm, setShowPriceForm] = React.useState(true)
+
     return (
         <ul>
             {ingredients.map(ingredient => (
@@ -106,15 +129,41 @@ function IngredientList({ ingredients }: { ingredients: Ingredient[] }) {
                             <Button variant="destructive" size="sm" type="submit" name="_action" value="ingredient-delete">
                                 <Trash2 size={16} />
                             </Button>
-                            <Button size="sm">
+                            <Button size="sm" onClick={() => setShowPriceForm(!showPriceForm)}>
                                 <MoreHorizontal size={16} />
                             </Button>
-                            {/* <Button size="sm" type="submit" name="_action" value="ingredient-add-price">Adiçionar Preço</Button> */}
                         </div>
                     </Form>
+                    <IngredientPriceForm id={ingredient.id} priceRecord={prices.find(p => p.ingredientId === ingredient.id)} />
                     <Separator />
                 </li>
             ))}
         </ul>
     )
+}
+
+function IngredientPriceForm({ id, priceRecord }: { id: string | undefined, priceRecord?: IngredientPrice }) {
+
+    return <Form method="post">
+        <Fieldset>
+            <Label htmlFor="ingredients-name">Nome</Label>
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>Fruits</SelectLabel>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+        </Fieldset>
+
+
+    </Form >
 }
