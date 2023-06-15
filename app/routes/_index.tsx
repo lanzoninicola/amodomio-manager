b721623fd10c891dcd9d4321461153ba03cdf4df
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2023-06-15 17:07:30 -0300

    feat: building home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,49 +2,86 @@ import type { V2_MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import Container from "~/components/layout/container/container";
 import { categoryEntity } from "~/domain/category/category.entity.server";
+import type { PizzaCatalog } from "~/domain/pizza-catalog/pizza-catalog.entity.server";
+import { pizzaCatalogEntity } from "~/domain/pizza-catalog/pizza-catalog.entity.server";
 import { ProductEntity } from "~/domain/product/product.entity";
 import type { Product } from "~/domain/product/product.model.server";
+import { sizeEntity } from "~/domain/size/size.entity.server";
 import { ok } from "~/utils/http-response.server";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Cardápio A Modio Mio" },
+    {
+      name: "description",
+      content: "Bem vindo ao cardápio da Pizza Delivery A Modo Mio",
+    },
   ];
 };
 
-export async function loader({ request }) {
+export async function loader() {
+  const productEntity = new ProductEntity();
+  const product = await productEntity.findAll();
+  const categories = await categoryEntity.findAll();
+  const sizes = await sizeEntity.findAll();
 
-  const productEntity = new ProductEntity()
-  const product = await productEntity.findAll()
+  const pizzaRawCatalog = (await pizzaCatalogEntity.findOne([
+    {
+      field: "type",
+      op: "==",
+      value: "pizza",
+    },
+  ])) as PizzaCatalog;
 
-  const categories = await categoryEntity.findAll()
+  let pizzaCatalog: any = [];
+
+  if (pizzaRawCatalog && pizzaRawCatalog.items) {
+    pizzaCatalog = pizzaRawCatalog.items.map((item) => {
+      return {
+        ...item.product,
+        product: {
+          ...product.find((p) => p.id === item.product.id),
+          sizes: item.product.sizes.map((s) => {
+            return {
+              ...s,
+              ...sizes.find((size) => size.id === s.id),
+              pizzas: s.toppings.map((topping) => {
+                return {
+                  ...topping,
+                  topping: {
+                    ...product.find((p) => p.id === topping.id),
+                  },
+                  category: {
+                    ...categories.find((c) => c.id === topping.categoryId),
+                  },
+                };
+              }),
+            };
+          }),
+        },
+      };
+    });
+  }
 
   return ok({
     product,
-    categories
-  })
+    categories,
+    sizes,
+    pizzaCatalog,
+  });
 }
-
-export async function action({ request }) {
-
-  return null
-}
-
-
-
-
-
 
 export default function HomePage() {
+  const loaderData = useLoaderData<typeof loader>();
+  const pizzaCatalog = loaderData.payload.pizzaCatalog;
 
-  const loaderData = useLoaderData<typeof loader>()
-  const products: Product[] = loaderData.payload.product
-  const categories = loaderData.payload.categories
+  console.log(pizzaCatalog);
 
   return (
     <Container>
-      <div className="grid place-items-center center w-full h-full text-4xl">This is the cardapio page</div>
+      <div className="grid place-items-center center w-full h-full text-4xl">
+        This is the cardapio page
+      </div>
     </Container>
-  )
+  );
 }
