f8873e24b4f7d527cdf9cd59daee5af29babcac1
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2023-06-16 21:12:04 -0300

    feat: working on firebase config

--- app/lib/firestore-model/src/config/get-firebase-config.server.ts
+++ app/lib/firestore-model/src/config/get-firebase-config.server.ts
@@ -1,7 +1,19 @@
-import configDev from "./amodomio-manager-dev.json";
-import configProd from "./amodomio-manager.json";
+const getFirebaseConfig = () => {
+  // Seems like the only key that is needed is the project_id
 
-const getFirebaseConfig = () =>
-  process.env.NODE_ENV === "production" ? configProd : configDev;
+  return {
+    // type: process.env.type,
+    project_id: process.env.project_id,
+    // private_key_id: process.env.private_key_id,
+    // private_key: process.env.private_key,
+    // client_email: process.env.client_email,
+    // client_id: process.env.client_id,
+    // auth_uri: process.env.auth_uri,
+    // token_uri: process.env.token_uri,
+    // auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    // client_x509_cert_url: process.env.client_x509_cert_url,
+    // universe_domain: process.env.universe_domain,
+  };
+};
 
 export default getFirebaseConfig;
