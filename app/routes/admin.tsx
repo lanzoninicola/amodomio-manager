2db144b6648c5870e567031d4df8848e41724d09
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2023-06-22 09:58:49 -0300

    feat: working on

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,6 +1,17 @@
+import { type LinksFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 
-
+export const links: LinksFunction = () => [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+    },
+    {
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap",
+        rel: "stylesheet",
+    },
+];
 
 
 export default function AdminOutlet() {
